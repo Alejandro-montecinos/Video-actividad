30cdbb9941c25bc5f5df60f9c3648f3e6c720a8e
Alejandro-montecinos <alejandromontecino002@gmail.com>
2025-04-23 20:27:54 -0400

    Se añande el spring boot

---
+++ VideoActividad/src/test/java/com/example/VideoActividad/VideoActividadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.VideoActividad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoActividadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
